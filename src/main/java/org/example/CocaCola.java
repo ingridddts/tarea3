b6ccd3cf1c3f6797fc7c02d357c431235077d825
Ingrid <131918653+ingridddts@users.noreply.github.com>
2023-06-03 03:44:34 -0400

    Update CocaCola.java
    docuemantacion

--- src/main/java/org/example/CocaCola.java
+++ src/main/java/org/example/CocaCola.java
@@ -1,11 +1,16 @@
-package org.example;
-//Clase CocaCola que hereda de clase abstracta bebida
+package org.example
+/**
+* Clase CocaCola que hereda de clase abstracta bebida
+*/
+
 class CocaCola extends Bebida{
     //constructor clase CocaCola
     public CocaCola(int i){
         super(i);
     }
-    //devuelve el sabor de la bebida, en ese caso coca cola
+    /**
+    * @return sabor de la bebida, en ese caso coca cola
+    */
     public String sabor(){
         String s = "cocacola";
         return s;
